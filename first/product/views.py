2cb23821749d274cbbae8cd1454f2ee63415f95e
Shuvo Halder <109753818+shuvo-sir@users.noreply.github.com>
2023-05-14 15:47:35 +0600

    Data base connect

--- first/product/views.py
+++ first/product/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from product.forms import RecentProduct
 from django.http import HttpResponseRedirect
+from .models import laptop
 
 
 # Create your views here.
@@ -10,6 +11,7 @@ def product(request):
 
 
 def send(request):
+
     return render(request, "Product/submit.html")
 
 
@@ -17,8 +19,17 @@ def details(request):
     if request.method == "POST":
         frm = RecentProduct(request.POST)
         if frm.is_valid():
-            print("Valid form")
-            print(frm.cleaned_data) 
+
+            pas = frm.cleaned_data["password"]
+            lap = frm.cleaned_data["laptop"]
+            eml = frm.cleaned_data["email"]
+            abt = frm.cleaned_data["about"]
+            tex = frm.cleaned_data["text_area"]
+            che = frm.cleaned_data["check_box"]
+            fil = frm.cleaned_data["files"]
+            buy = laptop(password = pas, laptop = lap, email = eml, about = abt, text_area = tex, check_box = che, files = fil)
+            buy.save()
+            
         return HttpResponseRedirect("/pro/su")
 
 
