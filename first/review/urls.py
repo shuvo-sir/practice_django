8fac6483cd26cc0ebf689af499a5fdbd053528cf
Shuvo Halder <109753818+shuvo-sir@users.noreply.github.com>
2023-05-17 02:48:24 +0600

    Forms
    Create the defults forms in django

--- first/review/urls.py
+++ first/review/urls.py
@@ -6,4 +6,5 @@ from . import views
 
 urlpatterns = [
     path("rew", views.customer, name = "review"),
+    path("build/", views.building_form),
 ]
--- first/review/views.py
+++ first/review/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-
-
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def customer(request):
     return render(request, "Review/review.html")
+
+
+
+def building_form(request):
+    if request.method == "POST":
+        frm = UserCreationForm(request.POST)
+        if frm.is_valid():
+            frm.save()
+
+    else:
+
+        frm = UserCreationForm
+    return render(request,"Review/building.html", {"form" : frm})
+
